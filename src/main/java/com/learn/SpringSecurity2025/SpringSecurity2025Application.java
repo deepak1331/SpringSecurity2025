b9849d40bb7635e41efdc5da793aa21698cd0717
Deepak Yadav <deepakyadav1331@gmail.com>
2025-08-07 16:12:03 +0530

    initial commit

---
+++ src/main/java/com/learn/SpringSecurity2025/SpringSecurity2025Application.java
@@ -0,0 +1,13 @@
+package com.learn.SpringSecurity2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/SpringSecurity2025/SpringSecurity2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.SpringSecurity2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
