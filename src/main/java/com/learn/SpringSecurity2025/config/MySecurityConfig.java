4cfd24bfca55015d9d302755f39ea602f86e5e50
Deepak Yadav <deepakyadav1331@gmail.com>
2025-08-08 17:44:36 +0530

    Adding own AuthenticationProvider

--- src/main/java/com/learn/SpringSecurity2025/config/MySecurityConfig.java
+++ src/main/java/com/learn/SpringSecurity2025/config/MySecurityConfig.java
@@ -1,22 +1,26 @@
 package com.learn.SpringSecurity2025.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class MySecurityConfig {
+
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -31,7 +35,18 @@ public class MySecurityConfig {
                         .build();
     }
 
+
     @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setUserDetailsService(userDetailsService);
+
+        return provider;
+    }
+
+    /*@Bean
     public UserDetailsService userDetailsService() {
 
         UserDetails user1 = User.withDefaultPasswordEncoder()
@@ -41,5 +56,5 @@ public class MySecurityConfig {
                 .username("Shipra").password("pass").roles("ADMIN", "USER").build();
 
         return new InMemoryUserDetailsManager(user1, user2);
-    }
+    }*/
 }
