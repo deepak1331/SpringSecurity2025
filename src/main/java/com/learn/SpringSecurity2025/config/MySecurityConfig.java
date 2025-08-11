e4d17bc5b0c75ecc7af775514471b063ad2bc424
Deepak Yadav <deepakyadav1331@gmail.com>
2025-08-11 16:27:34 +0530

    Whitelisted register and login API url's

--- src/main/java/com/learn/SpringSecurity2025/config/MySecurityConfig.java
+++ src/main/java/com/learn/SpringSecurity2025/config/MySecurityConfig.java
@@ -1,22 +1,27 @@
 package com.learn.SpringSecurity2025.config;
 
 import com.learn.SpringSecurity2025.service.MyUserDetailsService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.awt.event.WindowFocusListener;
 
 @Configuration
 @EnableWebSecurity
+@Log4j2
 public class MySecurityConfig {
 
 
@@ -28,8 +33,9 @@ public class MySecurityConfig {
         return
                 httpSecurity
                         .csrf(AbstractHttpConfigurer::disable)
-                        .authorizeHttpRequests(authorization -> authorization.requestMatchers("/register").permitAll())
-                        .authorizeHttpRequests(authorization -> authorization.anyRequest().authenticated())
+                        .authorizeHttpRequests(req -> req
+                                .requestMatchers("register", "login").permitAll()
+                                .anyRequest().authenticated())
                         //.formLogin(Customizer.withDefaults())
                         .httpBasic(Customizer.withDefaults())
                         .sessionManagement(session ->
@@ -41,22 +47,21 @@ public class MySecurityConfig {
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        //provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setPasswordEncoder(encoder());
         provider.setUserDetailsService(myUserDetailsService);
 
         return provider;
     }
 
-    /*@Bean
-    public UserDetailsService userDetailsService() {
 
-        UserDetails user1 = User.withDefaultPasswordEncoder()
-                .username("Avyaan").password("pass").roles("ADMIN", "USER").build();
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(BCryptPasswordEncoder.BCryptVersion.$2A, 10);
+    }
 
-        UserDetails user2 = User.withDefaultPasswordEncoder()
-                .username("Shipra").password("pass").roles("ADMIN", "USER").build();
-
-        return new InMemoryUserDetailsManager(user1, user2);
-    }*/
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
 }
